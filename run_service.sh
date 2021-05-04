47a29f7ef6acfba6baf71484627965abddb17865
Renan Campos <rcampos@redhat.com>
2021-05-04 16:10:40 -0400

    Running https server as a service in minikube.

---
+++ run_service.sh
@@ -0,0 +1,14 @@
+# Creates and runs the https server as a service in minikube.
+
+image="vaw-example"
+tag=$(date +%a-%H-%M-%S)
+pod="vaw-example"
+
+eval $(minikube docker-env)
+
+docker build -t $image:$tag .
+
+sed "s/IMAGE_NAME/$image:$tag/" yamls/deployment.yaml.template > yamls/deployment.yaml
+oc apply -f yamls/
+
+oc port-forward service/vaw-example 8080:9000
